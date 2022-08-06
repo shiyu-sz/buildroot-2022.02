cb32394b155fa1a4e28bb56cdd6a376e07baeb1b
shiyu <971586331@qq.com>
2022-08-06 18:50:26 +0800

    添加example_c例子

---
+++ package/example_c/example_c.mk
@@ -0,0 +1,24 @@
+################################################################################
+#
+# example_c
+#
+################################################################################
+ 
+EXAMPLE_C_VERSION:= 1.0.0
+EXAMPLE_C_SITE:= $(TOPDIR)/package/example_c
+EXAMPLE_C_SITE_METHOD:=local
+EXAMPLE_C_INSTALL_TARGET:=YES
+ 
+define EXAMPLE_C_BUILD_CMDS
+    $(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)" -C $(@D) all
+endef
+ 
+define EXAMPLE_C_INSTALL_TARGET_CMDS
+    $(INSTALL) -D -m 0755 $(@D)/example_c $(TARGET_DIR)/usr/bin
+endef
+ 
+define EXAMPLE_C_PERMISSIONS
+    /usr/bin/example_c f 4755 0 0 - - - - -
+endef
+ 
+$(eval $(generic-package))
---
+++ package/example_c/main.c
@@ -0,0 +1,8 @@
+
+#include <stdio.h>
+ 
+int main()
+{
+    printf("buildroot helloworld\n");
+    return 0;
+}
